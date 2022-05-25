96aa04b78704144fe5bcf5c0f5eef3421b21a5b1
Massimiliano Bonomi <massimiliano.bonomi@gmail.com>
2022-05-25 10:01:09 +0200

    wrong number

--- DATA/3-do_analysis.sh
+++ DATA/3-do_analysis.sh
@@ -22,7 +22,7 @@ rm traj_all.xtc
 # 4) PLUMED post-processing: RMSD calculation
 plumed driver --plumed ../../../DATA/plumed_driver.dat --mf_xtc ../2-PRODUCTION/traj-PBC.xtc
 
-# 6) Hydrogen bonds analysis
+# 5) Hydrogen bonds analysis
 # execute python script: requires python3 and MDAnalysis
 python ../../../DATA/count-HB.py ../../0-TOPO/topol_xtc.tpr ../2-PRODUCTION/traj-PBC.xtc
 
